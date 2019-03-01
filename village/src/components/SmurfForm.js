7a9c23fc71ec79f1ef300d587bd3e266330445bd
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-03-01 09:11:08 -0800

    Finished all axios requests and functionality, wrapping in react router now.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
@@ -13,13 +13,19 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+     axios
+        .post('http://localhost:3333/smurfs', {...this.state})
+        .then(res => this.setState({smurfs:res.data}))
+        .catch(err => console.log(err))
 
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
+      this.setState({
+        newFriend: {
+          name: "",
+          age: "",
+          email: ""
+        }
+      });
+}
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
